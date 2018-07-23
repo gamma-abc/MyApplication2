00ae830c9232584c9fb368f7272502facc075eeb
znkevin0 <znkevin0@gmail.com>
2018-07-23 10:10:14 +0800

    本地添加了一条注释

--- app/src/main/java/com/example/junk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/junk/myapplication/MainActivity.java
@@ -7,8 +7,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button mBtnTextView;
+    private Button mBtnEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //跳转到TEXT View
                 Intent intent=new Intent(MainActivity.this,twopage.class);
+                startActivity(intent);
+            }
+        });
+        mBtnEditText=findViewById(R.id.bt_4);
+        mBtnEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //跳转到登录界面
+                Intent intent=new Intent(MainActivity.this,sign_in.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/junk/myapplication/sign_in.java
@@ -0,0 +1,59 @@
+package com.example.junk.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class sign_in extends AppCompatActivity {
+    private Button mBtnLogin;
+    private EditText mEtUserName;
+    private ImageView mImageView;
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_longon);
+        mBtnLogin=findViewById(R.id.btn_login);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(sign_in.this,"登录成功",Toast.LENGTH_SHORT).show();
+            }
+        });
+        /**
+         * 监听事件
+         */
+        mEtUserName=findViewById(R.id.logon_user);
+        mEtUserName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //log.d("edittext", savedInstanceState.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        mImageView=findViewById(R.id.login_back);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(sign_in.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
